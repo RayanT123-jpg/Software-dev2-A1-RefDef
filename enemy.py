eb893f6d28edb6cf5b8ec24c51e95fce5e7b8dd2
RayanT123-jpg <rt24aaf@herts.ac.uk>
2026-06-22 03:39:45 +0100

    Update enemy.py

--- enemy.py
+++ enemy.py
@@ -1,1 +1,18 @@
+import pygame
+from settings import *
 
+class Enemy:
+    def __init__(self):
+        self.image = pygame.image.load(ASSET_PATH + "enemy.png")
+        self.image = pygame.transform.scale(self.image, (40, 40))
+        self.rect = self.image.get_rect(topleft=(600, 310))
+        self.direction = 2
+
+    def move(self):
+        self.rect.x += self.direction
+
+        if self.rect.left < 500 or self.rect.right > 750:
+            self.direction *= -1
+
+    def draw(self, screen):
+        screen.blit(self.image, self.rect)
